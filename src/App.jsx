0c68ec86e68a44e12569d86ab8552d2becd6d304
Sisi Wang <sisiwang242@gmail.com>
2023-11-30 17:32:59 -0500

    update fetch date exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import FetchDataExercise from './FetchData'
 import GiftCard from './GiftCard'
 
 function App() {
 
   return (
-    <div className='w-screen h-screen bg-gray-800 text-gray-200 flex justify-center items-center'>
+    <div className='w-screen h-screen bg-gray-800 text-gray-200 flex flex-col gap-6 justify-center items-center'>
       <GiftCard />
+      <FetchDataExercise />
     </div>
   )
 }
